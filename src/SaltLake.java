5fd83f07c42109876f34e2b7d7a1cfe393b485e4
geshaman <glebovyaroslav58@gmail.com>
2025-10-27 19:23:49 +0300

    Добавил геттеры и сеттеры

--- src/SaltLake.java
+++ src/SaltLake.java
@@ -5,6 +5,12 @@ class SaltLake extends Lake {
         super(name, area, salinity);
     }
 
+    public double getSaltProduction() {return saltProduction;}
+    public boolean getHasSaltIndustry() {return hasSaltIndustry; }
+
+    public void setSaltProduction(double count) { this.saltProduction = count; }
+    public void setHasSaltIndustry(boolean has) {this.hasSaltIndustry = has; }
+
     @Override
     public String getInfo() {
         return super.getInfo() + ", годовая добыча соли: " + saltProduction +
